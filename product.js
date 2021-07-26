494f44b5e4a3b0ce010e07d384d5be09da9b1a12
BoomBoomPiak <79363090+BoomBoomPiak@users.noreply.github.com>
2021-07-26 14:05:01 +0800

    keyboard-change product color

--- product.js
+++ product.js
@@ -1,3 +1,40 @@
 //this is for floating window when clicking the products
 
-//let user change the product color
+//let user change the product config
+
+// keyboard products change colour
+$(document).ready(function() {
+ 
+    $('.keyboard1-choose-color input').on('click', function() {
+        var headphonesColor = $(this).attr('data-image');
+   
+        $('.active').removeClass('active');
+        $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
+        $(this).addClass('active');
+    });
+   
+  });
+
+  $(document).ready(function() {
+ 
+    $('.keyboard2-choose-color input').on('click', function() {
+        var headphonesColor = $(this).attr('data-image');
+   
+        $('.active').removeClass('active');
+        $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
+        $(this).addClass('active');
+    });
+   
+  });
+
+  $(document).ready(function() {
+ 
+    $('.keyboard3-choose-color input').on('click', function() {
+        var headphonesColor = $(this).attr('data-image');
+   
+        $('.active').removeClass('active');
+        $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
+        $(this).addClass('active');
+    });
+   
+  });
