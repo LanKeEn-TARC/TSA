120370935dfccbffb11e37b5a36b7121781d14e7
LanKeEn-TARC <79739372+LanKeEn-TARC@users.noreply.github.com>
2021-08-02 12:54:40 +0800

    shipping fee done

--- delivery.js
+++ delivery.js
@@ -1,48 +1,53 @@
 const shippingDetails = [
     {
+         name: "nothing",
+         price: parseInt(0)},
+    {
         name: "Basic",
-        price: 5},
+        price: parseInt(5)},
     {
         name: "Standard",
-        price: 10},
-    {
-       name: "Premium",
-        price: 15}
-    ]
-    window.localStorage.setItem('fees', JSON.stringify(shippingDetails));
+        price: parseInt(10)},
 
-        
+        {
+            name: "Premium",
+            price: parseInt(15)},
+    
+    ];
+
+    var radios = document.getElementsByName('price')
+    for(var i = 0; i <radios.length; i++) 
+        radios[i].onclick = dispOrder;
+    
 function dispOrder(){
-    var btn = document.getElementsByClassName('price-btn')
 
     let html = '';
-    for(i=0; i<btn.length; i++){
-        if (document.getElementById('basic').checked) {
-            localStorage.setItem('fees', JSON.stringify(shippingDetails[0]))
-          } else if (document.getElementById('standard').checked){
+
+            if (document.getElementById('basic').checked) {
+            console.log(shippingDetails[1])
             localStorage.setItem('fees', JSON.stringify(shippingDetails[1]))
-        } else{
+
+           
+          } else if (document.getElementById('standard').checked){
+            console.log(shippingDetails[2])
             localStorage.setItem('fees', JSON.stringify(shippingDetails[2]))
+  
+        } else if (document.getElementById('premium').checked){
+            console.log(shippingDetails[3])
+            localStorage.setItem('fees', JSON.stringify(shippingDetails[3]))
+         
         }
-    }
-   
 
-    var x = localStorage.getItem('fees', JSON.stringify(shippingDetails))
-
-    
+        let shippingFee = JSON.parse(localStorage.getItem('fees'))
         html += `
-
-                <tfoot style="margin: auto;">
-                 
-                  <tr>
-                    <td>Delivery: </td> 
-                    <td>${x.price}</td>
-                  </tr>
-                
-                </tfoot>
-   </div>
-        `
-    ;
+        <tfoot style="margin: auto;">
+        <tr>
+          <td>Delivery: RM</td> 
+          <td>${shippingFee.price}</td>
+        </tr>
+        </tfoot>
+                `    
+    
    document.querySelector('.order-table').innerHTML = html;
 }
 dispOrder()
---
+++ delivery2.js
@@ -0,0 +1,6 @@
+console.log('Welcome to shiping cart TSA');
+const checkbox = document.querySelector('.price-btn input')
+
+console.log(checkbox);//check how many button available in document (debug propose)
+
+let delivery 
