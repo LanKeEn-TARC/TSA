82a66db680bbf7ed72718e62180a6134f8a666ae
LanKeEn-TARC <79739372+LanKeEn-TARC@users.noreply.github.com>
2021-08-07 15:26:56 +0800

    deleted

--- delivery2.js
+++
@@ -1,6 +0,0 @@
-console.log('Welcome to shiping cart TSA');
-const checkbox = document.querySelector('.price-btn input')
-
-console.log(checkbox);//check how many button available in document (debug propose)
-
-let delivery 
