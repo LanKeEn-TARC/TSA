78a914d247cd4a34fb15a3f1d0dc94e59b017c8f
BoomBoomPiak <79363090+BoomBoomPiak@users.noreply.github.com>
2021-08-10 13:57:00 +0800

    product-change-color

--- product.js
+++ product.js
@@ -1,45 +1,125 @@
-//this is for floating window when clicking the products
-
-//let user change the product config
-
 // keyboard products change colour
-$(document).ready(function() {
- 
-    $('.keyboard1-choose-color input').on('click', function() {
-        var headphonesColor = $(this).attr('data-image');
-   
-        $('.active').removeClass('active');
-        $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
-        $(this).addClass('active');
-    });
-   
-  });
+//keyboard1
+function changeSrcKeyboard1() {
+  if (document.getElementById("black-keyboard1").checked) {
+  document.getElementById("keyboard1-pic").src = "uncut-pic/ducky-black.jfif";
+  } 
+  else if (document.getElementById("white-keyboard1").checked) { 
+      document.getElementById("keyboard1-pic").src = "uncut-pic/ducky-white.jfif";
+  }
+}
 
-  $(document).ready(function() {
- 
-    $('.keyboard2-choose-color input').on('click', function() {
-        var headphonesColor = $(this).attr('data-image');
-   
-        $('.active').removeClass('active');
-        $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
-        $(this).addClass('active');
-    });
-   
-  });
+//keyboard2
+function changeSrcKeyboard2() {
+  if (document.getElementById("black-keyboard2").checked) {
+  document.getElementById("keyboard2-pic").src = "uncut-pic/razerblackwidowEliteBlack.png";
+  } 
+  else if (document.getElementById("pink-keyboard2").checked) { 
+      document.getElementById("keyboard2-pic").src = "uncut-pic/blackwidoweliteQuartz.jpg";
+  }
+}
 
-  $(document).ready(function() {
- 
-    $('.keyboard3-choose-color input').on('click', function() {
-        var headphonesColor = $(this).attr('data-image');
-   
-        $('.active').removeClass('active');
-        $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
-        $(this).addClass('active');
-    });
-   
-  });
+//keyboard3
+function changeSrcKeyboard3() {
+  if (document.getElementById("black-keyboard3").checked) {
+  document.getElementById("keyboard3-pic").src = "uncut-pic/razer-huntman-black.jfif";
+  } 
+  else if (document.getElementById("pink-keyboard3").checked) { 
+      document.getElementById("keyboard3-pic").src = "uncut-pic/razer-hunter-pink.png";
+  }
+}
 
-  //slide show
+//mouse1
+function changeSrcMouse1() {
+  if (document.getElementById("black-mouse1").checked) {
+  document.getElementById("mouse1-pic").src = "uncut-pic/coolmaster-black.jfif";
+  } 
+  else if (document.getElementById("white-mouse1").checked) { 
+      document.getElementById("mouse1-pic").src = "uncut-pic/coolmaster-white.jfif";
+  }
+}
+
+//console1
+function changeSrcConsole1() {
+  if (document.getElementById("black-console1").checked) {
+  document.getElementById("console1-pic").src = "uncut-pic/ps4-black.jpg";
+  } 
+  else if (document.getElementById("white-console1").checked) { 
+      document.getElementById("console1-pic").src = "uncut-pic/ps4-white.jfif";
+  }
+}
+
+//console2
+function changeSrcConsole2() {
+  if (document.getElementById("black-console2").checked) {
+  document.getElementById("console2-pic").src = "uncut-pic/ps4pro-black.jfif";
+  } 
+  else if (document.getElementById("white-console2").checked) { 
+      document.getElementById("console2-pic").src = "uncut-pic/ps4-pro-white.webp";
+  }
+}
+
+//console3
+function changeSrcConsole3() {
+  if (document.getElementById("disc-console3").checked) {
+  document.getElementById("console3-pic").src = "uncut-pic/ps5-disc.jpg";
+  } 
+  else if (document.getElementById("digital-console3").checked) { 
+      document.getElementById("console3-pic").src = "uncut-pic/ps5-digital.jfif";
+  }
+}
+
+//controller1
+function changeSrcController1() {
+  if (document.getElementById("black-controller1").checked) {
+  document.getElementById("controller1-pic").src = "uncut-pic/dualshock-black.jfif";
+  } 
+  else if (document.getElementById("red-controller1").checked) { 
+      document.getElementById("controller1-pic").src = "uncut-pic/dualshock-red.jfif";
+  }
+}
+
+//controller2
+function changeSrcController2() {
+  if (document.getElementById("black-controller2").checked) {
+  document.getElementById("controller2-pic").src = "uncut-pic/dualsense-black.jpg";
+  } 
+  else if (document.getElementById("white-controller2").checked) { 
+      document.getElementById("controller2-pic").src = "uncut-pic/dualsense-white.jfif";
+  }
+}
+
+//controller3
+function changeSrcController3() {
+  if (document.getElementById("black-controller3").checked) {
+  document.getElementById("controller3-pic").src = "uncut-pic/xbox-black.jfif";
+  } 
+  else if (document.getElementById("white-controller3").checked) { 
+      document.getElementById("controller3-pic").src = "uncut-pic/xbox-white.jpg";
+  }
+}
+
+//headphone2
+function changeSrcHeadphone2() {
+  if (document.getElementById("black-headphone2").checked) {
+  document.getElementById("headphone2-pic").src = "uncut-pic/sony-black.jfif";
+  } 
+  else if (document.getElementById("white-headphone2").checked) { 
+      document.getElementById("headphone2-pic").src = "uncut-pic/sony-white.jpg";
+  }
+}
+
+//chair1
+function changeSrcChair1() {
+  if (document.getElementById("black-chair1").checked) {
+  document.getElementById("chair1-pic").src = "uncut-pic/sakula-chair.jfif";
+  } 
+  else if (document.getElementById("red-chair1").checked) { 
+      document.getElementById("chair1-pic").src = "uncut-pic/sakula-red.webp";
+  }
+}
+
+//slide show
   var slideIndex = 1;
 showSlides(slideIndex);
 
