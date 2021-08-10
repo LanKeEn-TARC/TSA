9ea7ff9059e396364227a3c33031e56ac5ee2444
DankGuy <kelvinee@outlook.com>
2021-08-10 16:57:43 +0800

    itemDesc expand

--- faq.js
+++ faq.js
@@ -1,16 +1,33 @@
-window.addEventListener('scroll', reveal)
-
+// for animation
 function reveal () {
+  // get all elements with the class name grid-container
   var revealElements = document.querySelectorAll('.grid-container')
 
+  // to run the loop for the number of the element with the class name grid-container
   for (var i = 0; i < revealElements.length; i++) {
-    var windowTop = window.innerHeight
+    // get the height of the browser's window
+    var windowHeight = window.innerHeight
+    // get the element's box top position
+    // getBoundingClientRect() is to get size and position relative to the viewport while .top gets its top position
     var elementTop = revealElements[i].getBoundingClientRect().top
-
-    if (elementTop < windowTop) {
+    // if the relative top position of the element is lesser than the window height then it means the element's position is currently in the display
+    // else it means that it isn't in the displaying area
+    if (elementTop < windowHeight) {
+      // the pre-styled class is added to the elements
       revealElements[i].classList.add('animActive')
     } else {
+      // the pre-styled class is removed from the elements
       revealElements[i].classList.remove('animActive')
     }
   }
+}
+
+// to show the div for controller desc
+function controllerDescReveal () {
+  document.getElementById('controllerFaq').classList.toggle('faqQuestions-show')
+}
+
+window.addEventListener('scroll', reveal)
+document.getElementById('controllerExpandBtn').onclick = function () {
+  controllerDescReveal()
 }
