f9b46ba6d467599a209dff89b9e33eafeaa384cd
BoomBoomPiak <79363090+BoomBoomPiak@users.noreply.github.com>
2021-09-10 18:40:35 +0800

    cibai

--- product.js
+++ product.js
@@ -119,77 +119,19 @@ function changeSrcChair1() {
   }
 }
 
-
-//product modal box
-// Get the modal
-var modal =  document.getElementsByClassName('button');
-var btn = document.getElementsByClassName("myBtn");
-var span = document.getElementsByClassName("close");
-
-btn[0].onclick = function() {
-  modal[0].style.display = "block";
-}
-
-btn[1].onclick = function() {
-  modal[1].style.display = "block";
-}
-
-btn[2].onclick = function() {
-  modal[2].style.display = "block";
-}
-
-btn[3].onclick = function() {
-  modal[3].style.display = "block";
-}
-
-btn[4].onclick = function() {
-  modal[4].style.display = "block";
-}
-
-btn[5].onclick = function() {
-  modal[5].style.display = "block";
-}
-
-span[0].onclick = function() {
-  modal[0].style.display = "none";
-}
-
-span[1].onclick = function() {
-  modal[1].style.display = "none";
-}
-
-span[2].onclick = function() {
-  modal[2].style.display = "none";
-}
-
-span[3].onclick = function() {
-  modal[3].style.display = "none";
-}
-span[4].onclick = function() {
-  modal[4].style.display = "none";
-}
-
-span[5].onclick = function() {
-  modal[5].style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal[0]) {
-    modal[0].style.display = "none";
+function search() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("searchBox");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("searchList");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+      } else {
+          li[i].style.display = "none";
+      }
   }
-  if (event.target == modal[1]) {
-      modal[1].style.display = "none";
-  }  
-  if (event.target == modal[2]) {
-    modal[2].style.display = "none";
-  }
-  if (event.target == modal[3]) {
-    modal[3].style.display = "none";
-  }  
-  if (event.target == modal[4]) {
-    modal[4].style.display = "none";
-  }  
-  if (event.target == modal[5]) {
-    modal[5].style.display = "none";
-  }  
 }
