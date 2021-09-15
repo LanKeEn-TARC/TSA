8aeee301829f78e8f8c3d04d01f4eb66ae4d4936
Elly1130 <xychong-wp20@student.tarc.edu.my>
2021-09-16 01:20:46 +0800

    can have multiple account

--- Java-Script-Functions/Login_and_Signup.js
+++ Java-Script-Functions/Login_and_Signup.js
@@ -6,6 +6,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const PasswordInput = document.querySelector('.password');
     const ConfirmPassInput = document.querySelector('.confirmPassword');
     const ForgetPassEmail = document.querySelector('.forget_pass-input');
+    var proHistory = JSON.parse(localStorage.getItem('profileAccounts'));
+    var ProfileAcc = proHistory || [];
 
     //Switch from Login page to Signup page
     document.querySelector("#linkSignup").addEventListener("click", e => {
@@ -50,49 +52,60 @@ document.addEventListener("DOMContentLoaded", () => {
             return false;
         }
         if(username != null && email != null && password == confirmPassword){
-            localStorage.su_username = username;
-            localStorage.su_email = email;
-            localStorage.su_password = password;
-            alert("Signup sucessfully!");
-            return true;
+            let acc = {
+                acc_name: username,
+                acc_email: email,
+                acc_pass: password
+            }
+            addAccToLocalStorage(acc);
+            alert("Signup successfully!")
         }
     };
 
+    function addAccToLocalStorage(profileAccount){
+        ProfileAcc.push(profileAccount);
+        localStorage.setItem('profileAccounts', JSON.stringify(ProfileAcc));
+    }
+
     //Validation of username and password in login page
-    document.querySelector('.login-button').onclick = function(event){
-        
+    document.querySelector('.login-button').onclick = async function(event){
+
         event.preventDefault();
         var loginEmail = document.querySelector('.loginEmail').value,
         loginPassword = document.querySelector('.loginPassword').value;
 
-            if(loginEmail != localStorage.su_email && loginPassword != localStorage.su_password){
-                alert("Incorrect email and password combination! Please try again.");
-                return false;
-            }
-            else if(loginEmail == localStorage.su_email && loginPassword != localStorage.su_password){
-                alert("Incorrect password! Please try again.");
-                return false;
-            }
-            else if(loginEmail == localStorage.su_email && loginPassword == localStorage.su_password){
-                alert("Login sucessfully!");
-                //Link to home page
-                window.location.href="http://localhost:5501/homepage.html";
-            }
+        let allAcc = JSON.parse(localStorage.getItem('profileAccounts'));
+        var user = await allAcc.find(items => loginEmail == items.acc_email && loginPassword == items.acc_pass)
+        console.log(user);
+        if(user){
+            alert("Login Successfully!");
+            localStorage.su_email = user.acc_email;
+            localStorage.su_username = user.acc_name;
+            localStorage.su_password = user.acc_pass;
+            console.log(localStorage.su_email + localStorage.su_username + localStorage.su_password);
+            window.location.href = "http://Localhost:5501/Profile.html";
+        }
+        else{
+            alert("Invalid credential");
+        }
     };
 
     //Forget password
-    document.querySelector('.forget_pass-button').onclick = function(){
+    document.querySelector('.forget_pass-button').onclick = async function(event){
 
+        event.preventDefault();
         var forgetEmail = ForgetPassEmail.value;
+        let allAcc = JSON.parse(localStorage.getItem('profileAccounts'));
 
-            if(forgetEmail == localStorage.su_email){
-                alert("Dear " + localStorage.su_username + ", your password is " + localStorage.su_password);
-                return false;
-            }
-            if(forgetEmail != localStorage.su_email){
-                alert("Invalid email!");
-                return false;
-            }
+        var user = await allAcc.find(items => forgetEmail == items.acc_email)
+        console.log(user);
+        if(user){
+            alert("Dear " + user.acc_name + ", your password is " + user.acc_pass);
+            return false;
+        }
+        else{
+            alert("Invalid credential");
+        }
     }
 });
 
